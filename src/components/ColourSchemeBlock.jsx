14c13a068c21009654250f36632ff63ae0d132dd
Kareena Patel <kareenapat17@gmail.com>
2024-10-26 20:53:09 +0100

    Add logic to show copyable colour code + when code is copied
    Add new package
    
    Add styling to action icons

--- src/components/ColourSchemeBlock.jsx
+++ src/components/ColourSchemeBlock.jsx
@@ -1,8 +1,10 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, Fragment } from "react";
+import { Copy, Check } from "lucide-react";
 import { v4 as uuid } from 'uuid'
 
 export function ColourSchemeBlock({ mode, scheme }) {
   const [copiedValue, setCopiedValue] = useState(null)
+  const [hoveredValue, setHoveredValue] = useState(null)
 
   const copyToClipboard = useCallback((value) => {
     navigator.clipboard.writeText(value).then(() => {
@@ -19,12 +21,17 @@ export function ColourSchemeBlock({ mode, scheme }) {
           <li style={{ backgroundColor: colours.hex.value, color: colours.contrast.value }}
             key={uuid()}
             onClick={() => copyToClipboard(colours.hex.value)}
+            onMouseEnter={() => setHoveredValue(colours.hex.value)}
+            onMouseLeave={() => setHoveredValue(null)}
           >
             {colours.hex.value}
+            {copiedValue === colours.hex.value ? (
+              <Check className="colour-scheme-action"/>
+            ) : hoveredValue === colours.hex.value ? (
+              <Copy className="colour-scheme-action"/>
+            ) : null}
           </li>
-        )
-        }
-
+        )}
       </ul>
     </div>
   )
